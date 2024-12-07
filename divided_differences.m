95f83c4c2c028c7bfba138fd6802130c55ef7f77
RobertBenyamin <robertbenyamino@gmail.com>
2024-12-07 23:26:12 +0700

    Enhance divided differences function with table display

--- divided_differences.m
+++ divided_differences.m
@@ -3,11 +3,41 @@ function c = divided_differences(t, f)
     F = zeros(n, n);
     F(:,1) = f(:);
     
+    % Calculate divided differences - lower triangle
     for j = 2:n
-        for i = 1:(n-j+1)
-            F(i,j) = (F(i+1,j-1) - F(i,j-1))/(t(i+j-1) - t(i));
+        for i = j:n
+            F(i,j) = (F(i,j-1) - F(i-1,j-1))/(t(i) - t(i-j+1));
         end
     end
     
-    c = F(1,:);
+    % % Display table header
+    % fprintf('\nDivided Differences Table:\n');
+    % fprintf('%-10s ', 't');
+    % for j = 1:n
+    %     if j == 1
+    %         fprintf('%-15s ', 'f(t)');
+    %     else
+    %         fprintf('%-15s ', sprintf('f[%d]', j-1));
+    %     end
+    % end
+    % fprintf('\n');
+    
+    % % Display separator line
+    % fprintf('%s\n', repmat('-', 1, 10 + 15*n));
+    
+    % % Display table content - lower triangle
+    % for i = 1:n
+    %     fprintf('%-10.2f ', t(i));
+    %     for j = 1:i
+    %         fprintf('%-15.6e ', F(i,j));
+    %     end
+    %     fprintf('\n');
+    % end
+    % fprintf('\n');
+    
+    % Extract coefficients from main diagonal
+    c = zeros(1,n);
+    for i = 1:n
+        c(i) = F(i,i);
+    end
 end
